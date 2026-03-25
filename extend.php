26009be76eaced27f2df2800e26b4feac032b2c4
moyonie <718175115@qq.com>
2026-03-26 04:36:18 +0800

    Upload: extend.php

---
+++ extend.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of aifav/lang-chinese-simplified.
+ *
+ * Copyright (c) 2026 AI Favorites Team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Extend;
+
+return [
+    new Extend\LanguagePack(),
+];
